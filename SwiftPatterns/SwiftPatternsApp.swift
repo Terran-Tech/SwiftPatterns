4e3d1935158c69a974d04eaba782b38e8b3e4d02
Andre Dieb Martins <andre.dieb@gmail.com>
2024-01-18 15:04:49 -0300

    Fixed window size example

--- SwiftPatterns/SwiftPatternsApp.swift
+++ SwiftPatterns/SwiftPatternsApp.swift
@@ -3,8 +3,9 @@ import SwiftUI
 @main
 struct SwiftPatternsApp: App {
     var body: some Scene {
+        // Make window unresizable (fixedSize() + .contentSize)
         WindowGroup {
-            ContentView()
-        }
+            ContentView().fixedSize()
+        }.windowResizability(.contentSize)
     }
 }
