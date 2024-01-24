38c6357b9ddfe7aa5d791fc436b09159525252c7
Andre Dieb Martins <andre.dieb@gmail.com>
2024-01-24 20:42:18 -0300

    Add example of documentation

---
+++ SwiftPatterns/Examples/Documentation.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+// Basics of writing documentation.
+// Learn more: https://developer.apple.com/videos/play/wwdc2023/10244/
+
+/// Mines space rocks.
+class SpaceRockRobotMiner {
+
+    /// Instruct the robot to mine at a certain position. See also ``mineAround()``
+    func mine(x: Int, y: Int, z: Int) {
+    }
+
+    /// Instruct the robot to mine around its current position. See also ``mine(x:y:z:)``
+    func mineAround() {
+    }
+}
